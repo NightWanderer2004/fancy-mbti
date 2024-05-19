288945b010d15268d939c84859a08a466c76206c
Eduard Povierin <edpovieri@gmail.com>
2024-05-19 15:10:55 +0200

    Add feedback link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 export default function Footer() {
    return (
       <footer className='container mx-auto md:px-12 py-4 footer footer-center items-center gap-6 z-50'>
-         <nav className='flex gap-3'>
+         <nav className='flex gap-3 font-aspekta'>
             <a
                href='https://t.me/nightwanderer2004'
-               className='badge link-hover badge-secondary text-white badge-lg p-3'
+               className='px-3 py-1.5 rounded-lg border-none link-hover text-white bg-secondary'
                target='_blank'
                rel='noopener noreferrer'
             >
@@ -12,7 +12,7 @@ export default function Footer() {
             </a>
             <a
                href='https://github.com/NightWanderer2004'
-               className='badge link-hover bg-white badge-lg p-3 badge-outline'
+               className=' px-3 py-1.5 rounded-lg link-hover bg-white border-black border'
                target='_blank'
                rel='noopener noreferrer'
             >
@@ -20,13 +20,18 @@ export default function Footer() {
             </a>
             <a
                href='https://x.com/night2004_'
-               className='badge link-hover bg-black text-white badge-lg p-3 badge-outline'
+               className=' px-3 py-1.5 rounded-lg border-none link-hover bg-black text-white'
                target='_blank'
                rel='noopener noreferrer'
             >
                X
             </a>
+            /
+            <a href='https://insigh.to/b/fancy-mbti' target='_blank' className=' px-3 py-1.5 rounded-lg border-none link-hover bg-yellow-400'>
+               💡Feedback
+            </a>
          </nav>
+
          <p className='text-center text-accent underline'>
             <a href='/privacy'>All personal information on this site is not stored and kept private</a>
          </p>
