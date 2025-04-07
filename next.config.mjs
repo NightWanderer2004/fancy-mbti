ab0433d32add5288dd0f96df39d888cdf964f5da
Eduard Povierin <edpovieri@gmail.com>
2025-04-07 18:07:06 +0200

    Fixing

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    env: {
-      OPENAI_API_KEY: 'sk-proj-HivVl3Q3j3fxqtGUx6P1T3BlbkFJwvpIJ9ad4bJnmedbl0mm',
+      OPENAI_API_KEY: process.env.OPENAI_API_KEY,
    },
 }
 
